11fc692e889e66cd76282e6f4da2c6856e4d7735
WFlyToTheSky <wallyhare20@gmail.com>
2024-07-21 18:27:38 -0400

    First Commit

---
+++ src/main/java/net/wflytothesky/myfirstmod/MyFirstMod.java
@@ -0,0 +1,16 @@
+package net.wflytothesky.myfirstmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyFirstMod implements ModInitializer {
+	public static final String MOD_ID = "myfirstmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
