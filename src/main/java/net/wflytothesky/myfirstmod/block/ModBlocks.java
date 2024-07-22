261ba78c3e89cd4d027739b9300de4472703bc14
WFlyToTheSky <wallyhare20@gmail.com>
2024-07-22 16:13:32 -0400

    Added Ruby Block

--- src/main/java/net/wflytothesky/myfirstmod/block/ModBlocks.java
+++ src/main/java/net/wflytothesky/myfirstmod/block/ModBlocks.java
@@ -1,16 +1,20 @@
 package net.wflytothesky.myfirstmod.block;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.wflytothesky.myfirstmod.MyFirstMod;
 
 public class ModBlocks {
-
+    public static final Block RUBY_BLOCK = registerBlock("ruby_block",
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
 
 
     private static Block registerBlock(String name, Block block) {
