4cf690e4a1e3be0572f89cf80a8e631cef6a02ec
WFlyToTheSky <wallyhare20@gmail.com>
2024-07-23 17:44:08 -0400

    Added Sound Block

--- src/main/java/net/wflytothesky/myfirstmod/block/ModBlocks.java
+++ src/main/java/net/wflytothesky/myfirstmod/block/ModBlocks.java
@@ -13,12 +13,16 @@ import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.wflytothesky.myfirstmod.MyFirstMod;
+import net.wflytothesky.myfirstmod.block.custom.SoundBlock;
 
 public class ModBlocks {
     public static final Block RUBY_BLOCK = registerBlock("ruby_block",
             new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
     public static final Block RAW_RUBY_BLOCK = registerBlock("raw_ruby_block",
             new Block(FabricBlockSettings.copyOf(Blocks.RAW_IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+
+    public static final Block SOUND_BLOCK = registerBlock("sound_block",
+            new SoundBlock(FabricBlockSettings.copyOf(Blocks.RAW_IRON_BLOCK)));
 
     public static final Block RUBY_ORE = registerBlock("ruby_ore",
             new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.STONE).strength(2f), UniformIntProvider.create(3, 5)));
